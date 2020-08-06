54c3a77acae69ddef77857614c517c5d77063545
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-08-05 23:54:29 -0500

    Switched to is_running for s and gs

--- controls/vmtest.rb
+++ controls/vmtest.rb
@@ -1,6 +1,6 @@
 control 'azurerm_virtual_machine' do
   describe azurerm_virtual_machine(resource_group: 'myResourceGroup2', name: 'myVM') do
     it { should exist }
-    it { should be_running }
+    it { is_running }
   end
 end
