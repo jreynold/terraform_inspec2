4b65ee483d156d3581cbc0ad6af5420e4a969c99
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-08-05 23:19:35 -0500

    Name of IP address

--- controls/publiciptest.rb
+++ controls/publiciptest.rb
@@ -1,5 +1,6 @@
 control 'azurerm_public_ip' do
-  describe azurerm_public_ip(resource_group: 'myResourceGroup2', name: 'myVM') do
+  describe azurerm_public_ip(resource_group: 'myResourceGroup2', name: 'public_ip_address') do
+    it { should exist }
     its('properties.ipAddress') { should cmp '52.142.12.43' }
   end 
 end
