110f8e20c3ca41a0cc6be592ca2c1933ac0bd01b
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-08-05 23:23:17 -0500

    Correct cluster name

--- controls/publiciptest.rb
+++ controls/publiciptest.rb
@@ -1,5 +1,5 @@
 control 'azurerm_public_ip' do
-  describe azurerm_public_ip(resource_group: 'myResourceGroup2', name: 'public_ip_address') do
+  describe azurerm_public_ip(resource_group: 'myResourceGroup2', name: 'myPublicIP') do
     it { should exist }
     its('properties.ipAddress') { should cmp '52.142.12.43' }
   end 
