c408e113ac23a8ef065a1ce9714d6b2cef8182f1
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-08-05 23:47:20 -0500

    Switched to private, is this a bug?

--- controls/publiciptest.rb
+++ controls/publiciptest.rb
@@ -1,6 +1,6 @@
 control 'azurerm_network_interface' do
   describe azurerm_network_interface(resource_group: 'myResourceGroup2', name: 'myNIC') do
     it { should exist }
-    its('public_ip') { should cmp '52.142.12.43' }
+    its('private_ip') { should cmp '52.142.12.43' }
   end 
 end
