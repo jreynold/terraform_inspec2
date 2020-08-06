591992ac25d05d04743fa27aaa3b9d264761f60f
Jon Reynolds <jonreynolds@Jons-MacBook-Pro.local>
2020-08-05 22:58:34 -0500

    Trying to get it running

--- controls/vmtest.rb
+++ controls/vmtest.rb
@@ -1,10 +1,9 @@
-control 'vm up and running' do
+control 'azurerm_virtual_machine' do
   describe azurerm_virtual_machine(resource_group: 'myResourceGroup2', name: 'myVM') do
     it { should exist }
     it { should be_running }
   end
   describe azurerm_public_ip(resource_group: 'myResourceGroup2', name: 'myVM') do
     its('properties.ipAddress') { should cmp '52.142.12.43' }
-  end
-  
+  end 
 end
